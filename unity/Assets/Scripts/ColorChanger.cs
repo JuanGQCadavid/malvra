e1076b231a3e68909ad82ccdafe4305826d9c57f
Valentino <valentarmo@outlook.com>
2019-08-31 13:47:12 -0500

    Working example

---
+++ unity/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger: MonoBehaviour
+{
+    public MeshRenderer rend;
+    public Material[] mats;
+    public int cur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rend == null)
+        {
+            rend = GetComponent<MeshRenderer>();
+        }
+    }
+
+    public void Increment()
+    {
+        if (mats != null && mats.Length > 0)
+        {
+            cur = (cur + 1) % mats.Length;
+            if (rend != null)
+            {
+                rend.material = mats[cur];
+            }
+        }
+    }
+
+    public void RandomColor()
+    {
+        rend.material.color = UnityEngine.Random.ColorHSV();
+    }
+}
