5c496bbeefea37836e1247d2b7bc5526361dded9
Valentino <valentarmo@outlook.com>
2019-08-31 23:31:19 -0500

    Improved ActivityScene with orbital and added a 'menu' scene

---
+++ unity/Assets/Scripts/ActivityTimer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ActivityTimer : MonoBehaviour
+{
+    float timer;
+    float seconds;
+    float minutes;
+    float hours;
+
+    bool start;
+
+    private TMP_Text activityTimerText;
+
+    public void StartTimer()
+    {
+        start = true;
+    }
+
+    void TimerIncrement()
+    {
+        if (start)
+        {
+            timer += Time.deltaTime;
+            seconds = (int)(timer % 60);
+            minutes = (int)((timer / 60) % 60);
+            hours = (int)(timer / 3600);
+
+            activityTimerText = GetComponent<TMP_Text>();
+            activityTimerText.text = hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
+        }
+    }
+
+    public void StopTimer()
+    {
+        start = false;
+        timer = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        start = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerIncrement();
+    }
+}
---
+++ unity/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadDesbotonar()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadSiembraEnBanca()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadSiembraEnCama()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadCorte()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
