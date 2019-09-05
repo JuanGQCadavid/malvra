9acb04417b75e3f1209d91e3c5a1d5f2c01fb04b
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-09-05 13:13:03 -0500

    Adding task helper

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,6 +2,7 @@
 from database_bridge import Db_bridge
 #API
 from flask import Flask, request, jsonify
+from fake_backend import data
 import json
 import os
 
@@ -29,158 +30,6 @@ def add_worker():
 @app.route('/api/routines/add', methods=['POST'])
 def add_routine():
     return "To do ..."
-
-@app.route('/api/temp')
-def temp():
-    hola = {
-        "tasks": [
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Desbotonar",
-        "block": 41,
-        "date": "2019-09-04T08:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Siembra en Banca",
-        "block": 30,
-        "date": "2019-09-04T10:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Siembra en Cama",
-        "block": 42,
-        "date": "2019-09-04T14:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Corte",
-        "block": 38,
-        "date": "2019-09-04T16:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Corte",
-        "block": 38,
-        "date": "2019-09-04T08:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Corte",
-        "block": 25,
-        "date": "2019-09-04T10:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Siembra en Cama",
-        "block": 42,
-        "date": "2019-09-04T14:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Corte",
-        "block": 25,
-        "date": "2019-09-04T16:00:00.000000Z",
-        "completed": False,
-        "completion_time": None
-        },
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Desbotonar",
-        "block": 41,
-        "date": "2019-09-04T08:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Siembra en Banca",
-        "block": 30,
-        "date": "2019-09-04T10:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Siembra en Cama",
-        "block": 42,
-        "date": "2019-09-04T14:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "4321",
-        "employee_name": "Gregorio",
-        "task": "Corte",
-        "block": 38,
-        "date": "2019-09-04T16:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Corte",
-        "block": 38,
-        "date": "2019-09-04T08:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Corte",
-        "block": 25,
-        "date": "2019-09-04T10:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Siembra en Cama",
-        "block": 42,
-        "date": "2019-09-04T14:00:00.000000Z",
-        "completed": False,
-        "completion_time": ""
-        },
-        {
-        "employee_id": "1234",
-        "employee_name": "Maria",
-        "task": "Corte",
-        "block": 25,
-        "date": "2019-09-04T16:00:00.000000Z",
-        "completed": False,
-        "completion_time": None
-        }
-        ]
-    }
-    return jsonify(hola)
     
 
 @app.route('/api/test/workers')
@@ -190,11 +39,14 @@ def test_workers():
     db.add_workers('Karval', 'Carlos','Valencia','Karval@loscantoresdechipuco.tocapues')
 
     return get_workers()
+
 @app.route('/')
 def hello_world_web():
     return 'Yo, sup bro'
 
-
+@app.route('/api/temp/')
+def temp():
+    return jsonify(data)
 
 if __name__ == '__main__':
     dev_state, state = None,None
