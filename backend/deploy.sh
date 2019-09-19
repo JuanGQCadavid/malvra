48bbd52869cc5993d939c32de17aabf774dde60f
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-09-19 11:26:50 -0500

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,6 +7,11 @@ if ! [ -x "$(command -v docker)" ]; then
     yum install -y docker
     service docker start
     usermod -a -G docker ec2-user
+    echo "Done..."
+    echo "Installing Docker compose"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo "Done..."
 else
     echo 'Docker is installed'
 fi
